ec08c04b72bf56531d3dc578a5bd393de1dcde5a
jorden-niles <jordenniles@gmail.com>
2022-02-10 16:11:03 +0000

    JN: Initial commit

---
+++ BudgetApp.py
@@ -0,0 +1,124 @@
+# create class called budget and enter the attributes: self, food, clothing, entertainment
+# create a way for these objects to allow depositing and withdrawing funds (method) inside class
+
+class Budget:
+    def __init__(self, balance):
+        self.balance = balance
+
+    def withdraw(self, amount):
+        self.balance -= amount    
+
+    def deposit(self, amount):
+        self.balance += amount
+
+# def menu():
+#     menuInput = input("Enter (F)ood, \n (C)lothing, \n (E)ntertainment, \n (P)ets \n")
+#     if menuInput == 'f' or 'F':
+#         userAmount = input(float("Enter amount to withdraw: "))
+        
+#     return userAmount
+
+
+
+    # #if userInput == 'w' or 'W':
+    #     print("Which category would you like to withdraw from?: \n '\Food', \n 'Clothing' \n 'Entertainment', \n 'Pets' \n")
+    #     if userInput == "food":
+    #         print("Food balance:", food.balance)
+    #         userAmount = input("Enter amount to withdraw: ")
+    #         userAmount = int(userAmount)    
+
+# userInput = ""
+# userAmount = input("Enter amount to withdraw: ")
+
+def withdrawAmount(self, amount):
+    while userInput != 'q':
+    menuInput = input("Enter (F)ood, \n (C)lothing, \n (E)ntertainment, \n (P)ets,\n (Q)uit \n" )
+    if menuInput == 'f':
+        userAmount = input("Enter amount to withdraw: ")
+        userAmount = float(userAmount)
+        food.withdraw(userAmount)
+        print("Food balance:", food.balance)
+        break
+    elif menuInput == 'c':
+        userAmount = input("Enter amount to withdraw: ")
+        userAmount = float(userAmount)
+        clothing.withdraw(userAmount)
+        print("Clothing balance:", food.balance)
+        break
+    elif menuInput == 'e':
+        userAmount = input("Enter amount to withdraw: ")
+        userAmount = float(userAmount)
+        entertainment.withdraw(userAmount)
+        print("Entertainment balance:", food.balance)
+        break
+    elif menuInput == 'p':
+        userAmount = input("Enter amount to withdraw: ")
+        userAmount = float(userAmount)
+        pets.withdraw(userAmount)
+        print("Pets balance:", food.balance)
+        break
+    elif menuInput == "q":
+        break
+
+# menu()
+
+# setting the balance for each category
+food = Budget(50)
+clothing = Budget(20)
+entertainment = Budget(70)
+pets = Budget(12)
+
+userInput = ''
+
+while userInput != 'q':
+    menuInput = input("Enter (F)ood, \n (C)lothing, \n (E)ntertainment, \n (P)ets,\n (Q)uit \n" )
+    if menuInput == 'f':
+        userAmount = input("Enter amount to withdraw: ")
+        userAmount = float(userAmount)
+        food.withdraw(userAmount)
+        print("Food balance:", food.balance)
+        break
+    elif menuInput == 'c':
+        userAmount = input("Enter amount to withdraw: ")
+        userAmount = float(userAmount)
+        clothing.withdraw(userAmount)
+        print("Clothing balance:", food.balance)
+        break
+    elif menuInput == 'e':
+        userAmount = input("Enter amount to withdraw: ")
+        userAmount = float(userAmount)
+        entertainment.withdraw(userAmount)
+        print("Entertainment balance:", food.balance)
+        break
+    elif menuInput == 'p':
+        userAmount = input("Enter amount to withdraw: ")
+        userAmount = float(userAmount)
+        pets.withdraw(userAmount)
+        print("Pets balance:", food.balance)
+        break
+    elif menuInput == "q":
+        break
+
+print("Outside while loop")
+
+# # this goes into the withdraw method within the class and takes away the amount specified
+# food.withdraw(userAmount)
+# clothing.withdraw(userAmount)
+# entertainment.withdraw(userAmount)
+# pets.withdraw(userAmount)
+
+# print("Food balance:", food.balance)
+# print("Clothing balance:", clothing.balance)
+# print("Entertainment balance:", entertainment.balance)
+# print("Pet balance", pets.balance)
+
+# # this goes into the deposit method within the class and adds the amount specified
+# food.deposit(20)
+# clothing.deposit(10)
+# entertainment.deposit(100)
+# pets.deposit(8)
+
+# print("Food balance:", food.balance)
+# print("Clothing balance:", clothing.balance)
+# print("Entertainment balance:", entertainment.balance)
+# print("Pet balance", pets.balance)
